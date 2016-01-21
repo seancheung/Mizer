9f1b3b035ef0a714ed2c1f2d3b9134fb46b44551
Sean <theoxuanx@gmail.com>
2016-01-21 11:26:47 +0800

    Added vpt-repacker

--- Mizer/VirtualPlaytableWriter.cs
+++ Mizer/VirtualPlaytableWriter.cs
@@ -21,10 +21,26 @@ namespace Mizer.VirtualPlaytable
 
         public void WriteCards(Set set)
         {
-            using (var stream = File.CreateText(string.Format("{0}.{1}.vpt.xml", set.Name, set.Lang)))
+            if (set.Lang != "en" && File.Exists(string.Format("{0}.en.xml", set.Code.ToLower())))
             {
-                var ser = new XmlSerializer(typeof (Items));
-                ser.Serialize(stream, new Items(set));
+                var eng = File.ReadAllText(string.Format("{0}.en.xml", set.Code.ToLower()));
+                var engSet = (Set) new XmlSerializer(typeof (Set)).Deserialize(new StringReader(eng));
+                using (
+                    var stream =
+                        File.CreateText(string.Format("{0}.{1}.vpt.xml", engSet.Name,
+                            set.Lang.Replace("cn", "cs").Replace("tw", "ct").Replace("es", "sp").Replace("ko", "kr"))))
+                {
+                    var ser = new XmlSerializer(typeof (Items));
+                    ser.Serialize(stream, new Items(set, engSet));
+                }
+            }
+            else
+            {
+                using (var stream = File.CreateText(string.Format("{0}.{1}.vpt.xml", set.Name, set.Lang)))
+                {
+                    var ser = new XmlSerializer(typeof (Items));
+                    ser.Serialize(stream, new Items(set, set));
+                }
             }
         }
 
@@ -34,14 +50,18 @@ namespace Mizer.VirtualPlaytable
     [XmlRoot("items"), Serializable]
     public class Items : Set, IXmlSerializable
     {
+        public Set engSet;
         public Item[] items;
 
-        public Items(Set set)
+        public Items(Set set, Set engSet)
         {
+            this.engSet = engSet;
             Code = set.Code;
             Name = set.Name;
             Lang = set.Lang;
-            items = set.Cards.Select(c => new Item(c, set)).ToArray();
+            items = new Item[set.Cards.Length];
+            for (var i = 0; i < items.Length; i++)
+                items[i] = new Item(set.Cards[i], engSet.Cards[i], set);
         }
 
         protected Items()
@@ -63,10 +83,11 @@ namespace Mizer.VirtualPlaytable
         public void WriteXml(XmlWriter writer)
         {
             writer.WriteAttributeString("game", "mtg");
-            writer.WriteAttributeString("set", Code.ToUpper());
-            writer.WriteAttributeString("lang", Lang.ToUpper());
-            writer.WriteAttributeString("setname", Name);
-            writer.WriteAttributeString("imagepath", Name);
+            writer.WriteAttributeString("set", engSet.Code.ToUpper());
+            writer.WriteAttributeString("lang",
+                Lang.Replace("cn", "cs").Replace("tw", "ct").Replace("es", "sp").Replace("ko", "kr").ToUpper());
+            writer.WriteAttributeString("setname", engSet.Name);
+            writer.WriteAttributeString("imagepath", engSet.Name);
             writer.WriteAttributeString("date", "");
             writer.WriteAttributeString("border", "Black");
             writer.WriteAttributeString("copyright", "™ &amp; © 2015 Wizards of the Coast");
@@ -83,25 +104,27 @@ namespace Mizer.VirtualPlaytable
     [Serializable, XmlRoot("item")]
     public class Item : Card, IXmlSerializable
     {
+        public Card engCard;
         protected Set set;
 
         public bool IsLand
         {
-            get { return Regex.IsMatch(Type, @"\bLand\b", RegexOptions.IgnoreCase); }
+            get { return Regex.IsMatch(engCard.Type, @"\bLand\b", RegexOptions.IgnoreCase); }
         }
 
         public bool IsCreature
         {
-            get { return Regex.IsMatch(Type, @"\bCreature\b", RegexOptions.IgnoreCase); }
+            get { return Regex.IsMatch(engCard.Type, @"\bCreature\b", RegexOptions.IgnoreCase); }
         }
 
         public bool IsPlanesWalker
         {
-            get { return Regex.IsMatch(Type, @"\bPlaneswalker\b", RegexOptions.IgnoreCase); }
+            get { return Regex.IsMatch(engCard.Type, @"\bPlaneswalker\b", RegexOptions.IgnoreCase); }
         }
 
-        public Item(Card card, Set set)
+        public Item(Card card, Card engCard, Set set)
         {
+            this.engCard = engCard;
             this.set = set;
             Name = card.Name;
             Rarity = card.Rarity;
@@ -135,30 +158,31 @@ namespace Mizer.VirtualPlaytable
 
         public void WriteXml(XmlWriter writer)
         {
-            writer.WriteAttributeString("id", Name);
+            writer.WriteAttributeString("id", engCard.Name);
             if (IsLand)
-                writer.WriteAttributeString("ver", GetVer(this));
+                writer.WriteAttributeString("ver", GetVer(engCard));
             writer.WriteAttributeString("name", Name);
-            writer.WriteAttributeString("color", GetColor(this));
-            writer.WriteAttributeString("cost", GetFormatedManaCost(this));
-            writer.WriteAttributeString("cmc", ConvertedManaCost);
+            writer.WriteAttributeString("color", GetColor(engCard));
+            writer.WriteAttributeString("cost", GetFormatedManaCost(engCard));
+            writer.WriteAttributeString("cmc", engCard.ConvertedManaCost);
             writer.WriteAttributeString("type", Type);
-            writer.WriteAttributeString("types", GetFormatedType(this));
+            writer.WriteAttributeString("types", GetFormatedType(engCard));
             if (IsCreature)
             {
-                writer.WriteAttributeString("power", Power);
-                writer.WriteAttributeString("toughness", Toughness);
+                writer.WriteAttributeString("power", engCard.Power);
+                writer.WriteAttributeString("toughness", engCard.Toughness);
             }
             else if (IsPlanesWalker)
-                writer.WriteAttributeString("toughness", Loyalty);
-            var mana = GetMana(this);
+                writer.WriteAttributeString("toughness", engCard.Loyalty);
+            var mana = GetMana(engCard);
             if (!string.IsNullOrEmpty(mana))
                 writer.WriteAttributeString("mana", mana);
             writer.WriteAttributeString("text", GetFormatedText(this));
             writer.WriteAttributeString("flavor", Flavor);
-            writer.WriteAttributeString("artist", Artist);
-            writer.WriteAttributeString("number", GetFormatedNumber(this, set));
+            writer.WriteAttributeString("artist", engCard.Artist);
+            writer.WriteAttributeString("number", GetFormatedNumber(engCard, set));
             writer.WriteAttributeString("booster", "True");
+            writer.WriteAttributeString("rarity", engCard.Rarity);
             writer.WriteAttributeString("foil", "false|true");
         }
 
---
+++ VPT-Repacker/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace VPT_Repacker
+{
+    internal class Program
+    {
+        private static string mtg;
+        private static string decktypes;
+        private static string formats;
+        private static string packs;
+        private static string sets;
+        private static List<string> list = new List<string>();
+
+        private static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                var file = arg.Trim('"');
+                if (File.Exists(file))
+                {
+                    var name = Path.GetFileName(file);
+                    if (name == "mtg.jar")
+                    {
+                        mtg = file;
+                    }
+                    if (name == "decktypes.xml")
+                    {
+                        decktypes = file;
+                    }
+                    else if (name == "formats.xml")
+                    {
+                        formats = file;
+                    }
+                    else if (name == "packs.xml")
+                    {
+                        packs = file;
+                    }
+                    else if (name == "sets.xml")
+                    {
+                        sets = file;
+                    }
+                    else if (file.EndsWith(".vpt.xml"))
+                    {
+                        list.Add(file);
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(mtg))
+            {
+                Console.WriteLine("No mtg.jar package specified!");
+                do
+                {
+                    Console.WriteLine("mtg.jar path:");
+                    mtg = Console.ReadLine();
+                } while (string.IsNullOrEmpty(mtg) || !mtg.Trim('"').EndsWith("mtg.jar") || !File.Exists(mtg.Trim('"')));
+                mtg = mtg.Trim('"');
+            }
+
+            Console.WriteLine("following files will be repacked in: ");
+            if (!string.IsNullOrEmpty(decktypes))
+                Console.WriteLine(decktypes);
+            if (!string.IsNullOrEmpty(formats))
+                Console.WriteLine(formats);
+            if (!string.IsNullOrEmpty(packs))
+                Console.WriteLine(packs);
+            if (!string.IsNullOrEmpty(sets))
+                Console.WriteLine(sets);
+            foreach (var set in list)
+            {
+                Console.WriteLine(set);
+            }
+            Console.WriteLine("Press enter to confirm. Or add more files: ");
+            var input = Console.ReadLine();
+            while (!string.IsNullOrEmpty(input))
+            {
+                var file = input.Trim('"');
+                if (File.Exists(file))
+                {
+                    if (file.EndsWith("decktypes.xml"))
+                    {
+                        decktypes = file;
+                        Console.WriteLine("Added");
+                    }
+                    else if (file.EndsWith("formats.xml"))
+                    {
+                        formats = file;
+                        Console.WriteLine("Added");
+                    }
+                    else if (file.EndsWith("packs.xml"))
+                    {
+                        packs = file;
+                        Console.WriteLine("Added");
+                    }
+                    else if (file.EndsWith("sets.xml"))
+                    {
+                        sets = file;
+                        Console.WriteLine("Added");
+                    }
+                    else if (file.EndsWith(".vpt.xml"))
+                    {
+                        list.Add(file);
+                        Console.WriteLine("Added");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                }
+                Console.WriteLine("Press enter to confirm. Or add more files: ");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("processing...");
+            Process();
+            Console.ReadKey();
+        }
+
+        private static void Process()
+        {
+            if (!string.IsNullOrEmpty(decktypes) || !string.IsNullOrEmpty(formats) || !string.IsNullOrEmpty(packs) ||
+                !string.IsNullOrEmpty(sets) || list.Count > 0)
+                File.Copy(mtg, mtg.Replace("mtg.jar", "mtg.jar.backup"));
+            else
+            {
+                Console.WriteLine("Nothing to update...Done");
+            }
+
+            using (var zip = new ZipFile(mtg))
+            {
+                zip.BeginUpdate();
+                if (!string.IsNullOrEmpty(decktypes))
+                {
+                    zip.Add(decktypes, "database/decktypes.xml");
+                    Console.WriteLine("Added(Replaced) " + decktypes);
+                }
+                if (!string.IsNullOrEmpty(formats))
+                {
+                    zip.Add(formats, "database/formats.xml");
+                    Console.WriteLine("Added(Replaced) " + formats);
+                }
+                if (!string.IsNullOrEmpty(packs))
+                {
+                    zip.Add(packs, "database/packs.xml");
+                    Console.WriteLine("Added(Replaced) " + packs);
+                }
+                if (!string.IsNullOrEmpty(sets))
+                {
+                    zip.Add(sets, "database/sets.xml");
+                    Console.WriteLine("Added(Replaced) " + sets);
+                }
+                foreach (var set in list)
+                {
+                    zip.Add(set, "database/sets/" + Path.GetFileName(set).Replace(".vpt.xml", ".xml"));
+                    Console.WriteLine("Added(Replaced) " + set);
+                }
+                zip.CommitUpdate();
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
