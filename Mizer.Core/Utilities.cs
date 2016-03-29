7c9db5c6517dad647406a608baa3f3db25b46c6d
Sean <theoxuanx@gmail.com>
2016-03-29 09:46:41 +0800

    fixed connection termination issue

--- Mizer.Core/Utilities.cs
+++ Mizer.Core/Utilities.cs
@@ -11,6 +11,7 @@ namespace Mizer
         {
             using (var http = new WebClient())
             {
+                http.Headers["User-Agent"] = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E)";
                 http.Encoding = Encoding.UTF8;
                 return await http.DownloadStringTaskAsync(new Uri(url));
             }
@@ -20,6 +21,7 @@ namespace Mizer
         {
             using (var http = new WebClient())
             {
+                http.Headers["User-Agent"] = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E)";
                 http.Encoding = Encoding.UTF8;
                 return await http.DownloadDataTaskAsync(new Uri(url));
             }
@@ -29,6 +31,7 @@ namespace Mizer
         {
             using (var http = new WebClient())
             {
+                http.Headers["User-Agent"] = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E)";
                 http.Encoding = Encoding.UTF8;
                 await http.DownloadFileTaskAsync(new Uri(url), path);
             }
